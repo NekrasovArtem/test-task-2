b926eb6ebaf097b9fa186ef76ad8822c4e4a6a2f
Artem Losev <art3midus@yandex.com>
2024-05-13 17:21:03 +0500

    fix gulpfile + comments

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,19 @@
+// Подключение gulp и плагинов
 const { src, dest, watch, parallel, series} = require('gulp')
-
 const browserSync = require('browser-sync').create()
 const sass = require('gulp-sass')(require('sass'))
 const pug = require('gulp-pug')
 const useref = require('gulp-useref')
 const concat = require('gulp-concat')
 const uglify = require('gulp-uglify-es').default
-const imagemin = require('gulp-imagemin');
+const imagemin = require('gulp-imagemin')
+const clean = require('gulp-clean')
 
-
+// Сборка изображений в папку dist
 function images() {
     return src([
         'app/images/*',
-    ])
+    ], { encoding: false })
     .pipe(imagemin([
         imagemin.optipng(),
         imagemin.mozjpeg({
@@ -25,6 +26,7 @@ function images() {
     .pipe(dest('dist/images'))
 }
 
+// Сборка скриптов в один файл
 function scripts() {
     return src([
         'app/js/**/*.js',
@@ -36,6 +38,7 @@ function scripts() {
     .pipe(browserSync.stream())
 }
 
+// Сборка стилей в один файл
 function styles() {
     return src([
         'app/scss/**/*.scss',
@@ -47,6 +50,7 @@ function styles() {
     .pipe(browserSync.stream())
 }
 
+// Сборка pug в html
 function puged() {
     return src('app/*.pug')
     .pipe(pug({
@@ -57,11 +61,13 @@ function puged() {
     .pipe(browserSync.stream())
 }
 
+// Обновление pug-шаблонов (header, footer)
 function pugedIncludes() {
     return src('app/includes/*.pug')
     .pipe(browserSync.stream())
 }
 
+// Наблюдатель 
 function watching() {
     watch([
         'app/scss/**/*.scss',
@@ -78,6 +84,7 @@ function watching() {
     watch('app/*.pug', puged)
 }
 
+// Запуск dev-версии в браузере
 function browser() {
     browserSync.init({
         server: {
@@ -86,6 +93,7 @@ function browser() {
     })
 }
 
+// Сборка css/js/html в папку dist
 function building() {
     return src([
         'app/css/style.min.css',
@@ -95,6 +103,15 @@ function building() {
     .pipe(dest('dist'))
 }
 
+// Удаление папки dist
+function cleanDist() {
+    if (src('dist')) {
+        return src('dist', { read: false })
+        .pipe(clean())
+    }
+}
+
+// Экспорт функций
 exports.images = images
 exports.styles = styles
 exports.scripts = scripts
@@ -103,6 +120,10 @@ exports.pugedIncludes = pugedIncludes
 exports.watching = watching
 exports.browser = browser
 exports.building = building
+exports.cleanDist = cleanDist
 
-exports.build = series(building)
-exports.default = parallel(styles, scripts, pugedIncludes, puged, browser, watching)
+// Билд проекта
+exports.build = series(cleanDist, images, building)
+
+// Запуск dev-версии
+exports.default = parallel(styles, scripts, images, pugedIncludes, puged, browser, watching)
